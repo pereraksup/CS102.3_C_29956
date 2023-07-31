3871e09bff5f3c674cc2ab222ebbf5c8cd7b4eaa
pereraksup <135945510+pereraksup@users.noreply.github.com>
2023-07-31 13:20:32 +0530

    Add files via upload

--- Q4.c
+++ Q4.c
@@ -1,18 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
- char v;
- printf("Enter a character:\n");
- scanf("%c",&v);
- switch(v)
- {
- case 'a':printf("It is a vowel");break;
- case 'e':printf("It is a vowel");break;
- case 'i':printf("It is a vowel");break;
- case 'o':printf("It is a vowel");break;
- case 'u':printf("It is a vowel");break;
- default:printf("It is not a vowel");
- }
- return 0;
+  const float r,d,c, a, p = 3.14159;
+  printf("Enter The Radius Of The Circle - ");
+  scanf("%f", &r);
+  printf("The Diameter Of The Circle Is %f\n", 2.0 * r);
+  printf("The Circumference Of The Circle Is %f\n", 2.0 * p * r);
+  printf("The Area Of The Circle Is %f\n", p * r * r);
 }
-
--- Q5.c
+++ Q5.c
@@ -1,38 +1,14 @@
-#include <stdio.h>
+
+#include<stdio.h>
 int main()
 {
-int month;
-printf("Enter the month number (1-12): ");
-scanf("%d", &month);
-switch (month) {
-case 1:
-printf("January has 31 days.\n");break;
-case 2:
-printf("February has 28 days.\n");break;
-case 3:
-printf("March has 31 days.\n");break;
-case 4:
-printf("April has 30 days.\n");break;
-case 5:
-printf("May has 31 days.\n");break;
-case 6:
-printf("June has 30 days.\n");break;
-case 7:
-printf("July has 31 days.\n");break;
-case 8:
-printf("August has 31 days.\n");break;
-case 9:
-printf("September has 30 days.\n");break;
-case 10:
-printf("October has 31 days.\n");break;
-case 11:
-printf("November has 30 days.\n");break;
-case 12:
-printf("December has 31 days.\n");break;
-default:
-printf("Invalid month number.\n");break;
+  int m,n;
+  printf("Enter The First Integer - ");
+  scanf("%d",&m);
+  printf("\nEnter The Second Integer - ");
+  scanf("%d",&n);
+  if(m%n==0)
+    printf("\nThe First Integer Is A Multiple Of The Second Integer\n");
+  else
+    printf("\nThe First Integer Is Not A Multiple Of The Second Integer\n");
 }
-return 0;
-}
-
-
