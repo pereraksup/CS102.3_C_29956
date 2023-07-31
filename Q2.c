5f0a6be29102cf27d1e74a2e3d4ca3fd49364c86
pereraksup <135945510+pereraksup@users.noreply.github.com>
2023-07-31 13:30:07 +0530

    Add files via upload

--- Q2.c
+++ Q2.c
@@ -1,19 +1,43 @@
+
 #include<stdio.h>
 int main()
 {
-    int i=1,a,b=0;
-    float c;
-    while(i<=10)
-    {
-        printf("Enter The Marks Of Subject %d - ",i);
-        scanf("%d",&a);
-        i++;
-        b=b+a;
+    int h;
+    printf("Enter The Size For The Array 1 And Array 2 (Array 1 And Array 2 Both Should Have Equal Size) - ");
+    scanf("%d",&h);
+    int a[h],b[h],c,d,e,f[h],g[h],k;
+    printf("\nArray 1\n");
+    for(c=0;c<h;c++){
+        printf("Enter Value %d - ",c+1);
+        scanf("%d",&a[c]);
     }
-    c=(float)b/10.0;
-    printf("\n");
-    if((int)c<50)
-        printf("Fail\n");
-    else
-        printf("Pass\n");
+    printf("\nArray 2\n");
+    for(c=0;c<h;c++){
+        printf("Enter Value %d - ",c+1);
+        scanf("%d",&b[c]);
+    }
+    d=a[0];
+    e=b[0];
+    for(c=1;c<h;c++){
+        d=d+a[c];
+        e=e+b[c];
+
+    }
+    printf("\nScaler Sum Of Array 1 - %d\n",d);
+    printf("Scaler Sum Of Array 2 - %d\n",e);
+    printf("Vector Sum Of Array 1 And Array 2 - ");
+    for(c=0;c<h;c++){
+        f[c]=a[c]+b[c];
+        printf("%d ",f[c]);
+    }
+    printf("\nVector Product Of Array 1 And Array 2 - ");
+    for(c=0;c<h;c++){
+        g[c]=a[c]*b[c];
+        printf("%d ",g[c]);
+    }
+    k=g[0];
+    for(c=1;c<h;c++){
+            k=k+g[c];
+    }
+    printf("\nScaler Product Of Array 1 And Array 2 - %d\n",k);
 }
